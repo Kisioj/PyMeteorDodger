f7f909a182f5ef94ea14bc344810f409599a3b4d
Kisioj <kisioj@gmail.com>
2017-06-23 21:41:08 +0200

    Zastapiono spaceship_rect za pomoca obiektu spaceship typu GameObject

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import sys, pygame
 from pygame.locals import *
 
+class GameObject:
+    def __init__(self, image):
+        self.image = image
+        self.rect = image.get_rect()
+        
+    def render(self):
+        screen.blit(self.image, self.rect)
+
 pygame.init()
 size = width, height = 512, 768
 pygame.display.set_caption("Asteroid Dodger")
@@ -8,16 +16,14 @@ screen = pygame.display.set_mode(size)
 keys_down = set()
 clock = pygame.time.Clock()
 
-
 background_image = pygame.image.load("background.png").convert()
 background_rect = background_image.get_rect()
 
 spaceship_image = pygame.image.load("spaceship.png")
-spaceship_rect = spaceship_image.get_rect()
-spaceship_rect.y = height - spaceship_rect.h
+spaceship = GameObject(spaceship_image)
+spaceship.rect.y = height - spaceship.rect.h
 
-
-
+        
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,16 +33,16 @@ while 1:
         if event.type == KEYUP:
             keys_down.remove(event.key)
             
-    if K_LEFT in keys_down and spaceship_rect.x > 0:
-        spaceship_rect.x -= 8
-    if K_RIGHT in keys_down and spaceship_rect.x + spaceship_rect.w < width:
-        spaceship_rect.x += 8
+    if K_LEFT in keys_down and spaceship.rect.x > 0:
+        spaceship.rect.x -= 8
+    if K_RIGHT in keys_down and spaceship.rect.x + spaceship.rect.w < width:
+        spaceship.rect.x += 8
     
     background_rect.y += 4
     if background_rect.y >= 0:
         background_rect.y -= 768
 
     screen.blit(background_image, background_rect)
-    screen.blit(spaceship_image, spaceship_rect)
+    spaceship.render()
     pygame.display.flip()
     clock.tick(60)
