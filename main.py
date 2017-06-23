543723d806638401cc6adfecf28bcf7d9a250092
Kisioj <kisioj@gmail.com>
2017-06-23 21:58:56 +0200

    Dodano spadajace meteory i ich usuwanie po wylocie za ekran

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from pygame.locals import *
+import random
 
 class GameObject:
     def __init__(self, image):
@@ -23,6 +24,17 @@ spaceship_image = pygame.image.load("spaceship.png")
 spaceship = GameObject(spaceship_image)
 spaceship.rect.y = height - spaceship.rect.h
 
+meteor_images = [
+    pygame.image.load("meteor1.png"),
+    pygame.image.load("meteor2.png"),
+    pygame.image.load("meteor3.png"),
+    pygame.image.load("meteor4.png"),
+    pygame.image.load("meteor5.png"),
+    pygame.image.load("meteor6.png"),
+    pygame.image.load("meteor7.png"),
+    pygame.image.load("meteor8.png")
+]
+meteors = []
         
 while 1:
     for event in pygame.event.get():
@@ -38,11 +50,23 @@ while 1:
     if K_RIGHT in keys_down and spaceship.rect.x + spaceship.rect.w < width:
         spaceship.rect.x += 8
     
+    if random.randint(1, 30) == 1:
+        meteor_image = random.choice(meteor_images)
+        meteor = GameObject(meteor_image)
+        meteor.rect.x = random.randint(-50, 462)
+        meteor.rect.y = -meteor.rect.h
+        meteors.append(meteor)
+    
     background_rect.y += 4
     if background_rect.y >= 0:
         background_rect.y -= 768
-
+ 
     screen.blit(background_image, background_rect)
     spaceship.render()
+    for meteor in meteors:
+        meteor.rect.y += 8
+        meteor.render()
+    meteors = [meteor for meteor in meteors if meteor.rect.y < height]
+    
     pygame.display.flip()
     clock.tick(60)
