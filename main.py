efc7386de0d425e160301541a555b8e88f111800
Kisioj <kisioj@gmail.com>
2017-06-23 21:13:07 +0200

    Statek juz sie rusza z dobra predkoscia, ale na gorze mapki i wychodzi za granice

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ size = width, height = 512, 768
 pygame.display.set_caption("Asteroid Dodger")
 screen = pygame.display.set_mode(size)
 keys_down = set()
+clock = pygame.time.Clock()
+
 
 background_image = pygame.image.load("background.png").convert()
 background_rect = background_image.get_rect()
@@ -32,3 +34,4 @@ while 1:
     screen.blit(background_image, background_rect)
     screen.blit(spaceship_image, spaceship_rect)
     pygame.display.flip()
+    clock.tick(60)
