8e2005e7847e145a4478c1ce0c1db0cf5fc1e3d1
Kisioj <kisioj@gmail.com>
2017-06-23 20:15:22 +0200

    Wyswietlanie nieruchomego tla

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys, pygame
+from pygame.locals import *
 
 pygame.init()
 size = width, height = 512, 768
 pygame.display.set_caption("Asteroid Dodger")
 screen = pygame.display.set_mode(size)
 
+background_image = pygame.image.load("background.png").convert()
+background_rect = Rect(0, -768, 0, 0)
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+            
+    screen.blit(background_image, background_rect)
+    pygame.display.flip()
