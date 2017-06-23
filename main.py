cade003b1564cb58c768cd764c0e72c315e74c2f
Kisioj <kisioj@gmail.com>
2017-06-23 21:11:22 +0200

    Statek rusza sie na gorze mapy, za szybko i wychodzi poza jej granice

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ pygame.init()
 size = width, height = 512, 768
 pygame.display.set_caption("Asteroid Dodger")
 screen = pygame.display.set_mode(size)
+keys_down = set()
 
 background_image = pygame.image.load("background.png").convert()
 background_rect = background_image.get_rect()
@@ -13,10 +14,20 @@ spaceship_image = pygame.image.load("spaceship.png")
 spaceship_rect = spaceship_image.get_rect()
 
 
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == KEYDOWN:
+            keys_down.add(event.key)
+        if event.type == KEYUP:
+            keys_down.remove(event.key)
+            
+    if K_LEFT in keys_down:
+        spaceship_rect.x -= 8
+    if K_RIGHT in keys_down:
+        spaceship_rect.x += 8
             
     screen.blit(background_image, background_rect)
     screen.blit(spaceship_image, spaceship_rect)
