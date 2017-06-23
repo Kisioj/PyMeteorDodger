80278dd270f52373ec2fd1dbb5c11c85dabd09a5
Kisioj <kisioj@gmail.com>
2017-06-23 20:20:04 +0200

    Wyswietlanie statku w pozycji 0,0

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ screen = pygame.display.set_mode(size)
 background_image = pygame.image.load("background.png").convert()
 background_rect = background_image.get_rect()
 
+spaceship_image = pygame.image.load("spaceship.png")
+spaceship_rect = spaceship_image.get_rect()
+
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             
     screen.blit(background_image, background_rect)
+    screen.blit(spaceship_image, spaceship_rect)
     pygame.display.flip()
